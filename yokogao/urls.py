1ba746e2a23a1692c53ff2c5372a92cabf4f4652
Dhaval Savalia <dhaval.savalia6@gmail.com>
2017-09-18 01:02:27 +0530

    Update urls.py files with appropriate regexp

--- yokogao/urls.py
+++ yokogao/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'repos$', views.repo_list, name='repo_list'),
-    url(r'repo/(?P<repo_name>[\w-]+)', views.repo_detail, name='repo_detail'),
-    url(r'followers$', views.follower_list, name='follower_list'),
-    url(r'following$', views.following_list, name='following_list')
+    url(r'$repos/$', views.repo_list, name='repo_list'),
+    url(r'^repo/(?P<repo_name>[\w-]+)/$', views.repo_detail, name='repo_detail'),
+    url(r'^followers/$', views.follower_list, name='follower_list'),
+    url(r'^following/$', views.following_list, name='following_list')
 ]
